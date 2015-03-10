eb0ba0d7714e01a7c29993bc0532039c6768a126
PD1ce <padeisinger@gmail.com>
2015-03-10 14:24:26 -0500

    Carts moving on scene, swipes enabled, making animation functions

--- ScrollMaster/GameScene.swift
+++ ScrollMaster/GameScene.swift
@@ -6,40 +6,128 @@
 //  Copyright (c) 2015 PDice. All rights reserved.
 //
 
+import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let player = SKSpriteNode(imageNamed: "MinecartTemp")
+    var tracks: NSMutableArray!
+    let horizontalPositions = [CGFloat(128.0), CGFloat(256.0), CGFloat(384.0), CGFloat(512.0), CGFloat(640.0)] //Track 0, 1, 2, 3, 4
+    var playerPosition: Int!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+       
+        tracks = NSMutableArray()
+        for var i = 0; i < 5; i++ {
+            let track = SKSpriteNode(color: UIColor(red: 0.644, green: 0.164, blue: 0.164, alpha: 1.0), size: CGSize(width: self.frame.width, height: 32.0))
+            let realY = CGFloat((i + 1) * 128 - 32)
+            track.position = CGPoint(x: CGRectGetMidX(self.frame), y: realY)
+            println("Track \(i) X: \(track.position.x), Track \(i) Y: \(track.position.y)")
+            self.addChild(track)
+            tracks.addObject(track)
+        }
         
-        self.addChild(myLabel)
+        playerPosition = 2
+        
+        backgroundColor = SKColor(red: 0.3, green: 0.738, blue: 0.2, alpha: 1.0)
+        player.size = CGSize(width: 128.0, height: 96.0)
+        player.position = CGPoint(x: CGFloat(128.0), y: CGRectGetMidY(self.frame))
+        self.addChild(player)
+        /*
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addCart()),
+                SKAction.waitForDuration(2.0)
+            ])
+        */
+        
+        runAction(SKAction.repeatActionForever(
+            SKAction.sequence([
+                SKAction.runBlock(addCart),
+                SKAction.waitForDuration(1.0)
+            ])
+        ))
+        
+        // Not Used in this scene
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "swipeLeft:")
+        swipeLeft.direction = (UISwipeGestureRecognizerDirection.Left)
+        //self.view?.addGestureRecognizer(swipeLeft)
+        // Not Used in this scene
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swipeRight:")
+        swipeRight.direction = (UISwipeGestureRecognizerDirection.Right)
+        //self.view?.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "swipeUp:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view?.addGestureRecognizer(swipeUp)
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: "swipeDown:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view?.addGestureRecognizer(swipeDown)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: "tapped:")
+        self.view?.addGestureRecognizer(tapGesture)
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    func swipeUp(gr: UISwipeGestureRecognizer) {
+        println("Swipe Up")
+        if playerPosition != 4 {
+            //Executre MoveUp
+            player.position.y = horizontalPositions[playerPosition + 1]
+            playerPosition! += 1
+        }
+    }
+    func swipeDown(gr: UISwipeGestureRecognizer) {
+        println("Swipe Down")
+        if playerPosition != 0 {
+            player.position.y = horizontalPositions[playerPosition - 1]
+            playerPosition! -= 1
+        }
+    }
+    func swipeLeft(gr: UISwipeGestureRecognizer) {
+        println("Swipe Left")
+    }
+    func swipeRight(gr: UISwipeGestureRecognizer) {
+        println("Swipe Right")
+    }
+    
+    func tapped(gr: UITapGestureRecognizer) {
+        println("Tapped")
+    }
+    
+    func moveUp() {
+        
+    }
+    
+    func addCart() {
+        let cart = SKSpriteNode(imageNamed: "MinecartTemp")
+        cart.size = CGSize(width: 128.0, height: 96.0)
+        
+        let actualY = CGFloat(((rand() % 5) + 1) * 128)
+        //let actualY = CGFloat(96.0) //Will be random
+        cart.position = CGPoint(x: self.frame.size.width + cart.size.width/2, y: actualY)
+        addChild(cart)
+        println("Cart Y: \(cart.position.y)")
+        let cartDuration = CGFloat((rand() % 2) + 3) // Will be random
+        
+        let actionMove = SKAction.moveTo(CGPoint(x: -cart.size.width/2, y: actualY), duration: NSTimeInterval(cartDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        cart.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
     }
 }
